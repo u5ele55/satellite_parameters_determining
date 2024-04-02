69a1306399dc1463822a3f48ed0e9160bc7b71a3
Vyacheslav Shaganov <slavikshag@gmail.com>
2024-04-02 17:44:37 +0300

    visualization

--- visualize/visualize.py
+++ visualize/visualize.py
@@ -1,22 +1,25 @@
 from trajectory import *
 from geoid import GeoidDrawer
 from designations import DesignationsDrawer
+from designations_points import DesignationsPoints
 
 MAJOR_AXIS = 6378137; 
 MINOR_AXIS = 6356752.3142; 
 
 if __name__ == '__main__':
-    ax = plt.figure(1).add_subplot(projection='3d')
-    trdr = TrajectoryDrawer(r"trajectory.txt", MINOR_AXIS, MAJOR_AXIS)
-    geoidDr = GeoidDrawer(MINOR_AXIS, MAJOR_AXIS)
-    desDr = DesignationsDrawer(r"designations.txt")
+    # ax = plt.figure(1).add_subplot(projection='3d')
+    # trdr = TrajectoryDrawer(r"trajectory.txt", MINOR_AXIS, MAJOR_AXIS)
+    # geoidDr = GeoidDrawer(MINOR_AXIS, MAJOR_AXIS)
+    # desDr = DesignationsDrawer(r"designations.txt")
 
-    geoidDr.draw(ax)
-    trdr.prepareTrasse(r"../radiotelescopes.txt")
-    inECEF = False
-    trdr.draw(ax, inECEF, r"telescopes.txt")
+    # geoidDr.draw(ax)
+    # trdr.prepareTrasse(r"../radiotelescopes.txt")
+    # inECEF = False
+    # trdr.draw(ax, inECEF, r"telescopes.txt")
 
-    withDiscontinuties = True
-    desDr.draw(withDiscontinuties)
+    # withDiscontinuties = True
+    # desDr.draw(withDiscontinuties)
 
+    desp = DesignationsPoints("measurements.txt", "guess_measurements.txt", "result_measurements.txt", "time.txt")
+    desp.draw()
     plt.show()
