9d14e593a1c0a98addc37070ba6e64a408e0ec6f
Vyacheslav Shaganov <slavikshag@gmail.com>
2024-05-07 11:38:23 +0300

    changed measurements model

--- radiotelescopes/TelescopeControl.cpp
+++ radiotelescopes/TelescopeControl.cpp
@@ -1,7 +1,9 @@
 #include "TelescopeControl.hpp"
 
 #include "global/Constants.hpp"
+#include "LinAlg.hpp"
 #include "coordinates.hpp"
+#include "global/Constants.hpp"
 #include <cmath>
 
 TelescopeControl::TelescopeControl(
@@ -27,58 +29,81 @@ Vector TelescopeControl::targetTelescope(const Vector &ecef)
 
     auto r = ecef - rtCoord;
     double distanceSqr = r.dot(r);
+    
+    auto local = convertToLocalSK(ecef);
+    // return local;
+    
+        double dist = calculateDistance(local);
+        double az = calculateAzimuth(local);
+        double gamma = calculateTargetAngle(local);
 
-    if (distanceSqr < maxDistanceSqr) {
-        // find angle between RT plane and vector between RT and satellite
-        double cosAngle = r.dot(planeNormal) / r.norm();
-        if (cosAngle < 0) {
-            return {};
-        }
-        // acos is an angle between vector and plane
-        double angle = M_PI_2 - acos(cosAngle);
-        
-        if (angle > telescope.getAngle()) {
-            // could be seen!
-            double azimuth = calculateAzimuth(ecef, rtCoord); 
-            double distance = sqrt(distanceSqr); // in meters
-            // return ecef;
-            return {
-                convertToKm ? std::round(distance) / 1000.0 : distance,
-                convertToDegrees ? azimuth * 180 / M_PI : azimuth,
-                convertToDegrees ? angle * 180 / M_PI : angle 
-            };
-        } else {
-            // just in radiozone
-            // return {0};
-        }
-    }
+            return {dist, az, gamma};
+    
     return {}; 
 }
 
-double TelescopeControl::calculateAzimuth(Vector r_sat, Vector r_st)
+Vector TelescopeControl::convertToLocalSK(Vector r_sat)
 {
-    double k = planeNormal.dot(r_sat - r_st);
+    auto rtBLH = telescope.getBLH();
+    double B = rtBLH[0], L = rtBLH[1];
+    double cosB = cos(B), sinB = sin(B);
+    double cosL = cos(L), sinL = sin(L);
+    Matrix R = Matrix{
+        {-cosL*sinB,  -sinL*sinB,   cosB},
+        { cosL*cosB,   sinL*cosB,   sinB},
+        {-sinL,        cosL,        0}
+    };
+    Vector UEN = R * (r_sat - rtCoord);
 
-    // Projection of satellite on a station plane
-    auto l_p = r_sat - planeNormal*k;
-    Vector north(3);
+    return UEN;
 
-    if (r_st[2] == 0) {
-        north = {0,0,1};
-    } else if (r_st[2] < 0) {
-        double z_north_intersect = r_st.dot(r_st) / r_st[2];
-        north = r_st - Vector{0, 0, z_north_intersect};
-    } else {
-        double z_north_intersect = r_st.dot(r_st) / r_st[2];
-        north = Vector{0, 0, z_north_intersect} - r_st;
+    // auto E = LinAlg::Identity(3);
+    // Vector x_new = E[0], y_new = E[1], z_new = E[2];
+
+    // z_new = LinAlg::rotateAbout(z_new, x_new, -(rtBLH[0] - M_PI_2));
+    // y_new = LinAlg::rotateAbout(y_new, x_new, -(rtBLH[0] - M_PI_2));
+    
+    // double angle = LinAlg::angle(-rtCoord + Vector{0,0,Constants::Earth::MINOR_AXIS}, x_new);
+
+    // x_new = LinAlg::rotateAbout(x_new, z_new, -angle);
+    // y_new = LinAlg::rotateAbout(y_new, z_new, -angle);
+
+    // double st_x = r_sat.dot(x_new),
+    //        st_y = r_sat.dot(y_new),
+    //        st_z = r_sat.dot(z_new);
+
+    // return {st_x, st_z, st_y};
+}
+
+double TelescopeControl::calculateAzimuth(Vector r_sat)
+{
+    double x = r_sat[0], y = r_sat[1], z = r_sat[2];
+    
+    double azimuth;
+
+    if (fabs(x) < 1e-10) {
+        if (z > 0)
+            azimuth = M_PI_2;
+        else 
+            azimuth = M_PI; // mb 3 * M_PI_2
+    }
+    else {
+        azimuth = atan2(z, x);
+        if (azimuth < 0) {
+            azimuth = 2*M_PI + azimuth;
+        }
     }
 
-    double cosAzimuth = l_p[2] / sqrt(l_p.dot(l_p));
-
-    auto l_pCrossNorth = l_p.cross(north);
-    double cosNormCross = l_pCrossNorth.dot(planeNormal) / sqrt(l_pCrossNorth.dot(l_pCrossNorth));
-
-    double azimuth = cosNormCross > 0 ? acos(cosAzimuth) : 2*M_PI - acos(cosAzimuth);
-
     return azimuth;
+}
+
+double TelescopeControl::calculateTargetAngle(Vector r_sat)
+{
+    double x = r_sat[0], y = r_sat[1], z = r_sat[2];
+    return atan2(y, sqrt(x*x + z*z));
+}
+
+double TelescopeControl::calculateDistance(Vector r_sat)
+{
+    return r_sat.norm();
 }
