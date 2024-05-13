45a1f9c0a8a03fbc01968a49e2f34544fcada900
Vyacheslav Shaganov <slavikshag@gmail.com>
2024-05-13 17:22:52 +0300

    deleted unix time; detalization of JD-UTC convertion

--- utils/time.cpp
+++ utils/time.cpp
@@ -7,45 +7,6 @@
 
 #include <iostream>
 
-Vector unixToTime(long long secs) {
-    using namespace std::chrono;
-
-    system_clock::time_point tp{seconds{secs}};
-    time_t tt = system_clock::to_time_t(tp);
-    tm utc_tm = *gmtime(&tt);
-
-    Vector time = {
-        (double)utc_tm.tm_year + 1900,
-        (double)utc_tm.tm_mon + 1,
-        (double)utc_tm.tm_mday,
-        (double)utc_tm.tm_hour,
-        (double)utc_tm.tm_min,
-        (double)utc_tm.tm_sec,
-        0
-    };
-    time[6] = (
-        (time[3] * 60 + time[4]) * 60 + time[5]
-        ) / Constants::Earth::SECONDS_IN_DAY;
-
-    return time;
-}
-
-long long dateToUnix(Vector time) {
-    std::tm dt;
-    dt.tm_year = time[0] - 1900;
-    dt.tm_mon  = time[1] - 1;
-    dt.tm_mday = time[2];
-    dt.tm_hour = time[3];
-    dt.tm_min  = time[4];
-    dt.tm_sec  = time[5];
-    dt.tm_isdst = 0; // Not daylight saving
- 
-    std::time_t t = mktime(&dt); // Это "локальное время"
-    std::tm dt2 = *std::gmtime(&t);
-
-    return t + 3*60*60;
-}
-
 double dateToJDN(Vector date)
 {
     int Y = date[0], M = date[1], D = date[2];
@@ -82,15 +43,16 @@ Vector JDToTime(double JD) {
     double year = static_cast<int>(month > 2) ? c - 4716 : c - 4715;
 
     double hour = static_cast<int>(f * 24);
-    double minute = static_cast<int>((f * 24 - hour) * 60);
+    double minute = static_cast<int>( (f * 24 - hour) * 60 );
     double second = static_cast<int>( ((f * 24 - hour) * 60 - minute) * 60 );
+    double milsec = static_cast<int>( (((f * 24 - hour) * 60 - minute) * 60 - second) * 1000 );
 
-    return {year, month, day, hour, minute, second};
+    return {year, month, day, hour, minute, second, milsec};
 }
 
 double timeToJD(Vector date) {
     double JDN = dateToJDN(date);
-    double h = date[3], m = date[4], s = date[5];
-    double JD = JDN + (h-12) / 24.0 + m / 1440.0 + s / 86400.0;
+    double h = date[3], m = date[4], s = date[5], ms = date[6];
+    double JD = JDN + (h-12) / 24.0 + m / 1440.0 + s / 86400.0 + ms / 86400000.0;
     return JD;
 }
--- utils/time.hpp
+++ utils/time.hpp
@@ -1,8 +1,6 @@
 #pragma once
 #include "Vector.hpp"
 
-Vector unixToTime(long long secs);
-long long dateToUnix(Vector time);
 double dateToJDN(Vector date);
 double timeToJD(Vector date);
 Vector JDToTime(double JD);
