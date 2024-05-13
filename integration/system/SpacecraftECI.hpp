05d9823fb7f5635adeb2e1acb7b142b92f738f39
Vyacheslav Shaganov <slavikshag@gmail.com>
2024-05-13 13:07:39 +0300

    fixed comment

--- integration/system/SpacecraftECI.hpp
+++ integration/system/SpacecraftECI.hpp
@@ -6,8 +6,7 @@ class SpacecraftECI : public ISystem {
 public:
     /// @param mu geocentric gravitational constant 
     /// @param omega earth angular speed
-    /// @param initialPosition initial position of SC
-    /// @param initialSpeed initial speed of SC
+    /// @param initialState initial state of SC
     SpacecraftECI(double mu, double omega, const Vector& initialState);
 
     void f(Vector &state, double time) const;
