6b1bec71c533d4e42882ab15248e3274798151fc
Vyacheslav Shaganov <slavikshag@gmail.com>
2024-05-14 16:06:50 +0300

    support for negative time

--- integration/solver/AbstractSolver.cpp
+++ integration/solver/AbstractSolver.cpp
@@ -13,12 +13,27 @@ Vector AbstractSolver::solve(double time)
     Vector state(states[0].size());
     int stepsInTime = static_cast<int>(time / step);
 
+    if (time < 0) {
+        state = states[0];
+        for(int i = states.size() - 1; i < -stepsInTime; i++) {
+            methodStep(state, -step, i * step);
+        }
+        double wholePart = step * stepsInTime;
+        double timeDiff = time - wholePart;
+
+        if (timeDiff != 0) {
+            methodStep(state, timeDiff, time);
+        }
+
+        return state;
+    } 
+
     if (time < (states.size() - 1) * step) {
         state = states[stepsInTime];
     } else {
         state = states[states.size() - 1];
-        for(int _ = states.size() - 1; _ < stepsInTime; _++) {
-            methodStep(state, step, _ * step);
+        for(int i = states.size() - 1; i < stepsInTime; i++) {
+            methodStep(state, step, i * step);
             states.push_back(state);
         }
     }
